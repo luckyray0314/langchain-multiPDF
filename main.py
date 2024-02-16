59c36237a540467b2e0e9293a8fed21baa5d7992
saivietthanh0314 <saivietthanh0314@gmail.com>
2024-02-16 14:56:41 +1000

    rag_chain up

--- main.py
+++ main.py
@@ -51,6 +51,8 @@ qa_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+
+
 def load_chunk_persist_pdf() -> Chroma:
     pdf_folder_path = "data/"
     documents = []
@@ -92,6 +94,20 @@ def contextualized_question(input: dict):
     else:
         return input["question"]
 
+vectorstore = load_chunk_persist_pdf()
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k":6})
+# create the chain for allowing us to chat with the document
+
+rag_chain = (
+    RunnablePassthrough.assign(
+        context=contextualized_question | retriever
+    )
+    | qa_prompt
+    | llm
+)
+
+chat_history = []
 
 @app.route('/api/proprietary-assistant', methods = ['POST'])
 def proprietary_assistant():
@@ -124,18 +140,5 @@ def index():
 
 if __name__ == "__main__":
     print("kkk")
-    vectorstore = load_chunk_persist_pdf()
-
-    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k":6})
-    # create the chain for allowing us to chat with the document
-
-    rag_chain = (
-        RunnablePassthrough.assign(
-            context=contextualized_question | retriever
-        )
-        | qa_prompt
-        | llm
-    )
-
-    chat_history = []
+    
     app.run(host='0.0.0.0', port=5099)
